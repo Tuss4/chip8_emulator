4d2d8a9ee4b4d77ffb70c2338ad458d74ae33066
tuss4 <tuss4dbfn@gmail.com>
2014-11-06 22:49:54 -0600

    Kind of an update.

--- cpu.go
+++ cpu.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-var memory [4096]uint8 // Represents the vm's 4kb of RAM
+var memory [0x1000]uint8   // Represents the vm's 4kb of RAM
+var registers [0x10]uint16 // The 16 2 bit registers
 
-func main () {
-    for _, b := range memory {
-        fmt.Printf("%b", b)
-    }
-    fmt.Println(len(memory))
+func main() {
+	for _, b := range memory {
+		fmt.Printf("%b", b)
+	}
+	fmt.Println(len(memory))
+	fmt.Println(memory[0x200])
+	fmt.Println(registers)
 }
