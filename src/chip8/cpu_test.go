fb64574d71f34c5d7743b02cbee11387bfe43f04
tuss4 <tuss4dbfn@gmail.com>
2014-11-12 11:14:38 -0600

    Forgot to run the opcode function in the test. I'm not a well rested, man.

--- src/chip8/cpu_test.go
+++ src/chip8/cpu_test.go
@@ -4,7 +4,6 @@ import "fmt"
 import "testing"
 
 func TestOp_00EE(t *testing.T) {
-	var PC uint16
 	code := uint16(0x00EE)
 	Op_00EE(code)
 	if PC != stack[uint16(sp+uint8(1))] {
@@ -13,25 +12,22 @@ func TestOp_00EE(t *testing.T) {
 }
 
 func TestOp_1nnn(t *testing.T) {
-	var PC uint16
 	code := uint16(0x1ccc)
 	low_bits := code & 0xfff
 	Op_1nnn(code)
 	// PC should be equal to low_bits
 	if PC != low_bits {
 		t.Error("Wrong value set to the counter.")
-		fmt.Println(PC, low_bits)
 	}
 }
 
 func TestOp_2nnn(t *testing.T) {
-	var PC uint16
 	code := uint16(0x2bb5)
 	low_bits := code & 0xfff
+	Op_2nnn(code)
 	// PC should be equal to low_bits
 	if PC != low_bits {
 		t.Error("Wrong value set to counter")
-		fmt.Println(PC, low_bits)
 	}
 }
 
