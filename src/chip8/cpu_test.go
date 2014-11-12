899dad9de9acbcabe99b621d9a9e051b79f5f1d5
tuss4 <tuss4dbfn@gmail.com>
2014-11-12 11:10:39 -0600

    Op_1nnn, and Op_2nnn are failing. @scottferg thoughts?

--- src/chip8/cpu_test.go
+++ src/chip8/cpu_test.go
@@ -1,6 +1,37 @@
 package main
 
+import "fmt"
 import "testing"
+
+func TestOp_00EE(t *testing.T) {
+	var PC uint16
+	code := uint16(0x00EE)
+	Op_00EE(code)
+	if PC != stack[uint16(sp+uint8(1))] {
+		t.Error("Wrong value was set to the counter.")
+	}
+}
+
+func TestOp_1nnn(t *testing.T) {
+	var PC uint16
+	code := uint16(0x1ccc)
+	low_bits := code & 0xfff
+	Op_1nnn(code)
+	if PC != low_bits {
+		t.Error("Wrong value set to the counter.")
+		fmt.Println(PC, low_bits)
+	}
+}
+
+func TestOp_2nnn(t *testing.T) {
+	var PC uint16
+	code := uint16(0x2bb5)
+	low_bits := code & 0xfff
+	if PC != low_bits {
+		t.Error("Wrong value set to counter")
+		fmt.Println(PC, low_bits)
+	}
+}
 
 func TestOp_8xy2(t *testing.T) {
 	var register [0x10]uint8
