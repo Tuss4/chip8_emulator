e7177b24190b3eb405c6a68ddfc422a308fea2cf
Tuss4 <tuss4dbfn@gmail.com>
2014-12-27 01:10:13 -0600

    Drawing tests. W00t.

--- video.go
+++ video.go
@@ -17,13 +17,29 @@ func (v *Video) Initialize() {
 
 	defer sdl.Quit()
 
-	window := sdl.SetVideoMode(v.width, v.height, 32, sdl.OPENGL)
+	window := sdl.SetVideoMode(v.width, v.height, 32, sdl.SWSURFACE)
 	if window == nil {
 		log.Fatal(sdl.GetError())
 	}
 
+	image := sdl.Load("spidey.jpg")
+
+	window.Blit(nil, image, nil)
+
+	window.Flip()
+
 	sdl.WM_SetCaption(v.title, "")
-	sdl.Delay(3000)
+
+	rect := sdl.Rect{40, 40, 200, 200}
+
+	window.FillRect(&rect, sdl.MapRGB(window.Format, 0xff, 0xff, 0xff))
+
+	window.Flip()
+
+	sdl.Delay(1000)
+
+	image.Free()
+
 	return
 
 }
